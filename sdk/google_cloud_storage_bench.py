1950c4c2db27f451e075b13c3d1956d1fe0f7430
Jackie Tung <jackie@outerbounds.co>
2022-07-27 13:56:00 -0700

    add some options

--- sdk/google_cloud_storage_bench.py
+++ sdk/google_cloud_storage_bench.py
@@ -1,5 +1,6 @@
 import io
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+import argparse
 
 from google.cloud import storage
 
@@ -43,14 +44,13 @@ def generate_randbytes(size_mb: int):
     return result
 
 
-SIZE_MB = 500
 PARALLEL_BLOB_OPERATIONS = 10
 TEST_BLOB_FN = lambda n: "test-blob-%d" % n
 TEST_BUCKET = "ob_gcp_exploration"
 
 
-def simple_download(i):
-    with Measure('download', SIZE_MB) as m:
+def simple_download(i, args):
+    with Measure('download', args.size_mn) as m:
         storage_client = storage.Client()
         bucket = storage_client.bucket(TEST_BUCKET)
         print("Downloading blob %s" % TEST_BLOB_FN(i))
@@ -59,9 +59,9 @@ def simple_download(i):
     return m.rate
 
 
-def simple_upload(i):
-    data = bytes(generate_randbytes(SIZE_MB))
-    with Measure('upload', SIZE_MB) as m:
+def simple_upload(i, args):
+    data = bytes(generate_randbytes(args.size_mb))
+    with Measure('upload', args.size_mb) as m:
         storage_client = storage.Client()
         bucket = storage_client.bucket(TEST_BUCKET)
         print("Uploading to blob %s" % TEST_BLOB_FN(i))
@@ -72,18 +72,26 @@ def simple_upload(i):
 
 
 def main():
-    executor = ThreadPoolExecutor(max_workers=PARALLEL_BLOB_OPERATIONS)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use-processes', action='store_true')
+    parser.add_argument('--parallelism', type=int, default=10)
+    parser.add_argument('--size-mb', type=int, default=500)
+    args = parser.parse_args()
+    if args.use_processes:
+        executor = ProcessPoolExecutor(max_workers=args.parallelism)
+    else:
+        executor = ThreadPoolExecutor(max_workers=args.parallelism)
     ups = []
-    for i in range(PARALLEL_BLOB_OPERATIONS):
-        ups.append(executor.submit(simple_upload, i))
+    for i in range(args.parallelism):
+        ups.append(executor.submit(simple_upload, i, args))
     up_rates = [up.result() for up in ups]
     # If task executions become staggered, this aggregation will become over optimistic.
     # No staggering observed to date, so keeping it dead simple for now
     # TODO(jackie) make it robust
     up_msg = "Aggregate upload rate: %.2f Mbps" % (sum(up_rates) * 8)
     downs = []
-    for i in range(PARALLEL_BLOB_OPERATIONS):
-        downs.append(executor.submit(simple_download, i))
+    for i in range(args.parallelism):
+        downs.append(executor.submit(simple_download, i, args))
     down_rates = [down.result() for down in downs]
     # If task executions become staggered, this aggregation will become over optimistic.
     # No staggering observed to date, so keeping it dead simple for now
